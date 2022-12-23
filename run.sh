2750f6302bce2091732d0bd201e69902db7e5d9d
jangkrikletoy <121164360+jangkrikletoy@users.noreply.github.com>
2022-12-23 22:13:28 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ clear
 echo "Katacoda Centos Windows 11 by fb.com/thuong.hai.581"
 echo "Paste authtoken here: "
 read CRP
-./ngrok authtoken $CRP 
+./ngrok authtoken "2J65suyCs4Ccko0e0ettgXgp9sI_7F2o8cwPoNxCkUmpYF53C" 
 nohup ./ngrok tcp --region eu 30889 &>/dev/null &
 yum install sudo -y
 echo "Downloading QEMU"
