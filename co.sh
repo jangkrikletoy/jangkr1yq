651e73687ac75565534330c451fe0eeb6fed0d82
jangkrikletoy <121164360+jangkrikletoy@users.noreply.github.com>
2022-12-24 23:15:05 +0700

    Update co.sh

--- co.sh
+++ co.sh
@@ -1,17 +1,1 @@
-hour=0
-min=0
-sec=10
-echo sec
-        while [ $hour -ge 0 ]; do
-                 while [ $min -ge 0 ]; do
-                         while [ $sec -ge 0 ]; do
-                                 echo -ne "$hour:$min:$sec\033[0K\r"
-                                 let "sec=sec-1"
-                                 sleep 1
-                         done
-                         sec=59
-                         let "min=min-1"
-                 done
-                 min=59
-                 let "hour=hour-1"
-         done
+echo "ok"
