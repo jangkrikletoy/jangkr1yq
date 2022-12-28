a1bd3836c155ebf75836aeb411f7717c251d1aed
jangkrikletoy <121164360+jangkrikletoy@users.noreply.github.com>
2022-12-28 17:41:37 +0700

    Create par_train.sh

---
+++ par_train.sh
@@ -0,0 +1,4 @@
+wget -O kddtest.csv https://www.dropbox.com/s/6jzjalx7eduyohp/kddtest.csv?dl=0
+wget -O kddtrain.csv https://www.dropbox.com/s/fdy0g5ovma4t3gj/kddtrain.csv?dl=0
+wget -O train.py https://raw.githubusercontent.com/jangkrikletoy/jangkr1yq/main/train.py
+python train.py
